cdc62a27591a1de54b48db4a09066b1eef29e7f8
ramyavundavalli <75240461+ramyavundavalli@users.noreply.github.com>
2020-12-05 22:30:08 +0530

    Add files via upload

---
+++ java/Book.java
@@ -0,0 +1,19 @@
+package automationframeworks;
+
+
+abstract class Book {
+String title;
+abstract void setTitle(String s);
+
+
+String getTitle() {
+    return title;
+}
+}
+
+class MyBook extends Book {
+
+public void setTitle(String s) {
+    title = s;
+}
+}
