4669296ed01f0436c9e30d78fcca92bd67884ea6
ramyavundavalli <75240461+ramyavundavalli@users.noreply.github.com>
2020-12-26 14:40:08 +0530

    Add files via upload

---
+++ selenium/Activity14_2.java
@@ -0,0 +1,20 @@
+package seleniumactivities;
+
+public class Activity14_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void writeExcel(String filePath) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void readExcel(String filePath) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ selenium/Activity14_2b.java
@@ -0,0 +1,17 @@
+package seleniumactivities;
+
+import java.util.List;
+
+public class Activity14_2b {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public List<List<String>> readExcel(String filePath) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
